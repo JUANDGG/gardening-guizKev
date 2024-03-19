aac7d07a0988b62db0d42d5fea66a4796c7cee30
juandgg <juanguiza65@gmail.com>
2024-03-19 12:54:57 -0500

    feat: :sparkles: new cors configuration

--- src/main/java/com/guizKev/api/segurity/ConfigCors.java
+++ src/main/java/com/guizKev/api/segurity/ConfigCors.java
@@ -13,7 +13,7 @@ public class ConfigCors {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://apigardeningguizkev-production.up.railway.app/api", "http://127.0.0.1:5500")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
